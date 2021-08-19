73237495091cee672da004b875586e08879ba22a
AlaaPro <valaa4@gmail.com>
2021-08-19 16:09:53 +0100

    create Components Main / Header / Basket / Product + create the App UI/Dom  + add onAdd & onRemove functions

---
+++ src/data.js
@@ -0,0 +1,27 @@
+const data = {
+  products: [
+    {
+      id: '1',
+      name: 'macBook',
+      price: 1400,
+      image:
+        'https://i.picsum.photos/id/0/5616/3744.jpg?hmac=3GAAioiQziMGEtLbfrdbcoenXoWAW-zlyEAMkfEdBzQ',
+    },
+    {
+      id: '2',
+      name: 'Old Car',
+      price: 2400,
+      image:
+        'https://i.picsum.photos/id/1002/4312/2868.jpg?hmac=5LlLE-NY9oMnmIQp7ms6IfdvSUQOzP_O3DPMWmyNxwo',
+    },
+    {
+      id: '3',
+      name: 'W SHoses',
+      price: 1000,
+      image:
+        'https://i.picsum.photos/id/1010/5184/3456.jpg?hmac=7SE0MNAloXpJXDxio2nvoshUx9roGIJ_5pZej6qdxXs',
+    },
+  ],
+};
+
+export default data;
